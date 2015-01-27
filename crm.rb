5685c1030f4b066800b52908fbff0f39fddf7f7b
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-27 15:13:08 -0500

    Add Code To Add In New Contact From Contact Forum
    Add in code to add in new contact from the contact forum

--- crm.rb
+++ crm.rb
@@ -17,3 +17,12 @@ end
 get "/contacts/new" do
 	erb :new_contact
 end
+
+post "/contacts" do
+	# for testing do not add when adding code to actully projects
+	puts params
+
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	$rolodex.add_contact(new_contact)
+	redirect to("/contacts")
+end
