8a3bc16d79f685cb63d9f17ee842855bc3d4b79d
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-29 14:20:20 -0500

    Add Put Method Request To Update Contact in CRM

--- crm.rb
+++ crm.rb
@@ -53,3 +53,18 @@ post "/contacts" do
 	$rolodex.add_contact(new_contact)
 	redirect to("/contacts")
 end
+
+put "/contacts/:id" do
+	@contact = @@rolodex.search_contact(params[:id].to_i)
+
+	if @contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]
+		@contact.note = params[:note]
+
+		redirect to("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
+end
